1e53d22b9100785f4d7a951e6460d0f7d002c242
Vanessa Cedeno <vanessaicm@gmail.com>
2012-10-19 17:13:52 -0500

    Aplicacion ruby on rails nueva

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vanessaicm::Application.config.secret_token = '530bfe18beb9b083ac28d9f563b5c3eceaeee324e21aa043307dcc0c65dd9bc0d031e8f1dc08b1ad45288bc0e5f965d6136a97d868a430d8dcfaeb03f482ee99'
