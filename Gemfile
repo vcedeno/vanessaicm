e7a78958ef94e3aeddcbb75a2fe3253abafdb566
vanessa cedeno <vanessaicm@nina>
2013-06-19 10:39:46 -0500

    fix

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.8'
 
 gem 'pg'
 gem 'heroku'
+gem 'thin'
 #gem 'sqlite3'
 
 # Gems used only for assets and not required
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Vanessaicm::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
